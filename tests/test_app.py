4b6a57e89eb1250c93af9da430875adf66516f12
Yijin Wang <yijin.wang2003@gmail.com>
2026-01-24 13:48:33 -0600

    Fix flake8 E402 by restructuring imports

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,8 @@
-import sys
 import os
+import sys
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
+sys.path.insert(0, PROJECT_ROOT)
 
 from app import app
 
