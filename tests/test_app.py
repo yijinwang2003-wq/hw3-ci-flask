057c022fd4d297590121b07a04ce843adc8d5d10
Yijin Wang <yijin.wang2003@gmail.com>
2026-01-24 12:41:15 -0600

    Format code with Black to satisfy CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,29 @@
 from app import app
 
+
 def test_health():
     client = app.test_client()
     resp = client.get("/health")
     assert resp.status_code == 200
+
 
 def test_hello():
     client = app.test_client()
     resp = client.get("/hello")
     assert resp.status_code == 200
 
+
 def test_add():
     client = app.test_client()
     resp = client.get("/add")
     assert resp.status_code == 200
 
+
 def test_health_content():
     client = app.test_client()
     resp = client.get("/health")
     assert resp.get_json()["status"] == "ok"
+
 
 def test_hello_content():
     client = app.test_client()
